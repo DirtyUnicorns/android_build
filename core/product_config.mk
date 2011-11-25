b8d195cef19c8bd290a1e49780d2006b63eb695c
Ricardo Cerqueira <github@cerqueira.org>
2011-11-25 15:30:36 +0000

    Preliminary support for product builds
    Change-Id: I2a5e70dda973a6fcdbba0d5e26b35b99d3f1aea2

--- core/product_config.mk
+++ core/product_config.mk
@@ -184,9 +184,14 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-# Read in all of the product definitions specified by the AndroidProducts.mk
-# files in the tree.
-all_product_configs := $(get-all-product-makefiles)
+  ifneq ($(DU_BUILD),)
+    $(call import-products, device/du/$(DU_BUILD)/device.mk)
+    all_product_configs := device/du/$(DU_BUILD)/device.mk
+  else
+    # Read in all of the product definitions specified by the AndroidProducts.mk
+    # files in the tree.
+    all_product_configs := $(get-all-product-makefiles)
+  endif # DU_BUILD
 endif
 
 # Find the product config makefile for the current product.
@@ -240,6 +245,16 @@ $(call import-products, $(current_product_makefile))
 endif  # Import all or just the current product makefile
 
 # Sanity check
+=======
+  # Read in all of the product definitions specified by the AndroidProducts.mk
+    # files in the tree.
+    #
+    #TODO: when we start allowing direct pointers to product files,
+    #    guarantee that they're in this list.
+    $(call import-products, $(get-all-product-makefiles))
+  endif
+endif # TARGET_BUILD_APPS
+>>>>>>> build: Preliminary support for DU product builds
 $(check-all-products)
 
 ifneq ($(filter dump-products, $(MAKECMDGOALS)),)
