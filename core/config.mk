832ee5d87d3a7d5ff2b691656c17bd8139b90993
Dorian Snyder <dastin1015@gmail.com>
2014-07-09 22:29:35 -0700

    build: this is DU not CM
    Change-Id: Ia6ecf87be5424568ed06913cf2c33ab747ef0fda

--- core/config.mk
+++ core/config.mk
@@ -497,7 +497,7 @@ RS_PREBUILT_COMPILER_RT := prebuilts/sdk/renderscript/lib/$(TARGET_ARCH)/libcomp
 # Rules for QCOM targets
 include $(BUILD_SYSTEM)/qcom_target.mk
 
-ifneq ($(CM_BUILD),)
+ifneq ($(DU_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include vendor/du/sepolicy/sepolicy.mk)
