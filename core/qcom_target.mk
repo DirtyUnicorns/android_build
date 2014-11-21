30ae7d573d4ed532872092074183ab0c01490d46
Ethan Chen <intervigil@gmail.com>
2014-11-21 11:51:03 -0800

    Allow devices to specify a RIL variant
    Change-Id: Ia5d35d916be358fb7be7ead6bd05cc3628b35a37

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -4,6 +4,9 @@
 define qcom-set-path-variant
 $(call project-set-path-variant,qcom-$(2),TARGET_QCOM_$(1)_VARIANT,hardware/qcom/$(2))
 endef
+define ril-set-path-variant
+$(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/$(1))
+endef
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
@@ -31,7 +34,7 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     2ND_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
     2ND_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
     2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
-    2ND_CLANG_TARGET_GLOBAL_CFLAGS += $(qcom_flags)
+    2ND_CLANG_TARGET_GLOBAL_CPPFLAGS += $(qcom_flags)
 
 $(call project-set-path,qcom-audio,hardware/qcom/audio-caf/$(TARGET_BOARD_PLATFORM))
 ifeq ($(USE_DEVICE_SPECIFIC_CAMERA),true)
@@ -43,6 +46,7 @@ $(call project-set-path,qcom-display,hardware/qcom/display-caf/$(TARGET_BOARD_PL
 $(call qcom-set-path-variant,GPS,gps)
 $(call project-set-path,qcom-media,hardware/qcom/media-caf/$(TARGET_BOARD_PLATFORM))
 $(call qcom-set-path-variant,SENSORS,sensors)
+$(call ril-set-path-variant,ril)
 else
 $(call project-set-path,qcom-audio,hardware/qcom/audio/default)
 $(call qcom-set-path-variant,CAMERA,camera)
@@ -50,4 +54,5 @@ $(call project-set-path,qcom-display,hardware/qcom/display/$(TARGET_BOARD_PLATFO
 $(call qcom-set-path-variant,GPS,gps)
 $(call project-set-path,qcom-media,hardware/qcom/media/default)
 $(call qcom-set-path-variant,SENSORS,sensors)
+$(call ril-set-path-variant,ril)
 endif
