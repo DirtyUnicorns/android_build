f10acac8c007a5c36d1f5d8d599708ed9335c86c
Iftekhar Rifat <rifat662@gmail.com>
2015-07-04 00:22:00 +0000

    Squashed commit for chromium_prebuilt (2/3)
    build: Add chromium prebuilt support to envsetup.sh && The core Makefile
    
    This adds a chromium_prebuilt function to envsetup.sh that is invoked by lunch to check
    whether the chromium prebuilts are up-to-date or not. If not, it will be built from source
    and then the new source built version will be pulled during brunch/mka bacon to become the
    new prebuilts for future builds.
    
    This is all opt-in through the USE_PREBUILT_CHROMIUM flag. Without it being set to 1,
    none of this would be ran, and regular operations will go on.
    
    PS13:
    -use export TARGET_DEVICE
    -replace git -C with params compatible to old git versions
    
    Change-Id: I40f8c8dbd2a8a84a5c1b9f47ee04180a71ef4e07
    
    build: Fix PreBuilt Chromium refs
    
    Change-Id: Icc553d5be1e71d8c754bc85b6906252e5690b508

--- envsetup.sh
+++ envsetup.sh
@@ -635,8 +635,14 @@ function lunch()
 
     echo
 
+    if $USE_PREBUILT_CHROMIUM
+    then
+        chromium_prebuilt
+    fi
+
     set_stuff_for_environment
     printconfig
+
 }
 
 # Tab completion for lunch.
@@ -1986,6 +1992,20 @@ function pez {
     return $retval
 }
 
+function chromium_prebuilt() {
+    T=$(gettop)
+    export TARGET_DEVICE=$(get_build_var TARGET_DEVICE)
+    hash=$T/prebuilts/chromium/$TARGET_DEVICE/hash.txt
+
+    if [ -r $hash ] && [ $(git --git-dir=$T/external/chromium_org/.git --work-tree=$T/external/chromium_org rev-parse --verify HEAD) == $(cat $hash) ]; then
+        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=yes
+        echo "** Prebuilt Chromium is up-to-date; Will be used for build **"
+    else
+        export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=no
+        echo "** Prebuilt Chromium out-of-date/not found; Will build from source **"
+    fi
+}
+
 function get_make_command()
 {
   echo command make
@@ -2018,8 +2038,6 @@ function make()
     echo
     return $ret
 }
-
-
 
 if [ "x$SHELL" != "x/bin/bash" ]; then
     case `ps -o command -p $$` in
