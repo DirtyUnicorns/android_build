9c0550dd9082d0792149cd2924b812338470f74c
Susan McCombs <smac0628@gmail.com>
2015-10-09 22:34:14 -0400

    Remove SuperSU
    - Fix roomservice
    
    Change-Id: I0c4cc63f4b78d494680a40fe72fc6b76a259809a

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -737,11 +737,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
       script.Unmount("/system")
 
   if block_based:
-    script.Print("Flashing SuperSU...")
-    common.ZipWriteStr(output_zip, "supersu/supersu.zip",
-                   ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
-    script.FlashSuperSU()
-  if block_based:
      script.Print("Flashing Boot Image...")
      script.WriteRawImage("/boot", "boot.img")
 
