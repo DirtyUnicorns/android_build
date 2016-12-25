581a11dbca0d0f6f6119b4e9b2f6a0f276ca4249
beanstown106 <nbenis106@gmail.com>
2016-12-25 13:40:14 -0500

    Set new pixel sounds as defaults
    - Slightly modified to use the DU vendor instead of an extra vendor
    
    Change-Id: Id9806d641adf22b741d9d4bc09b1c5e3ebd2bf2a

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -32,15 +32,19 @@ PRODUCT_PACKAGES += \
     LiveWallpapersPicker
 
 # Additional settings used in all AOSP builds
+ifeq ($(wildcard vendor/du/google/GoogleAudio.mk),)
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.config.ringtone=Ring_Synth_04.ogg \
     ro.config.notification_sound=pixiedust.ogg
+endif
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
+ifeq ($(wildcard vendor/du/google/GoogleAudio.mk),)
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
+endif
 
 # Get the TTS language packs
 $(call inherit-product-if-exists, external/svox/pico/lang/all_pico_languages.mk)
