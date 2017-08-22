aea85f3fe174dd64ef6571167d665a787fd2246e
Koushik Dutta <koushd@gmail.com>
2017-08-22 23:10:40 +0200

    Change how we search for DU makefiles
    Change-Id: I00fdb8f425f675bff23924620bedb7d1c6a1b987

--- core/product_config.mk
+++ core/product_config.mk
@@ -174,8 +174,7 @@ all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
  ifneq ($(DU_BUILD),)
-    $(call import-products, device/du/$(DU_BUILD)/device.mk)
-    all_product_configs := device/du/$(DU_BUILD)/device.mk
+    all_product_configs := device/*/$(DU_BUILD)/du.mk
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
