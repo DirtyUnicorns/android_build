f9a44f8d8c6a645aa7a7da17fdd144f7c5931e00
Guillaume Lesniak <me@xplod.fr>
2017-08-26 12:12:51 -0400

    Add brunch/breakfast aliases
    Change-Id: Ic4f15556a37fdf2e3a376c2f6dc9718c90a3a1a6

--- envsetup.sh
+++ envsetup.sh
@@ -569,6 +569,49 @@ function print_lunch_menu()
     echo
 }
 
+function breakfast()
+{
+    target=$1
+    DU_DEVICES_ONLY="true"
+    unset LUNCH_MENU_CHOICES
+    add_lunch_combo full-eng
+    for f in `/bin/ls vendor/du/vendorsetup.sh 2> /dev/null`
+        do
+            echo "including $f"
+            . $f
+        done
+    unset f
+
+    if [ $# -eq 0 ]; then
+        # No arguments, so let's have the full menu
+        lunch
+    else
+        echo "z$target" | grep -q "-"
+        if [ $? -eq 0 ]; then
+            # A buildtype was specified, assume a full device name
+            lunch $target
+        else
+            # This is probably just the du model name
+            lunch du_$target-userdebug
+        fi
+    fi
+    return $?
+}
+
+alias bib=breakfast
+
+function brunch()
+{
+    breakfast $*
+    if [ $? -eq 0 ]; then
+        time mka bacon
+    else
+        echo "No such item in brunch menu. Try 'breakfast'"
+        return 1
+    fi
+    return $?
+}
+
 function lunch()
 {
     local answer
