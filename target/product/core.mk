17974cf7d97d7a02df478c7e7ada16179dad726e
Alex Cruz <du.alexcruz@gmail.com>
2017-08-31 23:09:40 +0200

    Remove AOSP Calendar
    Change-Id: I52a5852bbfdf67b119593a69791933c7c9a138aa

--- target/product/core.mk
+++ target/product/core.mk
@@ -25,7 +25,6 @@ PRODUCT_PACKAGES += \
     BlockedNumberProvider \
     BookmarkProvider \
     BuiltInPrintService \
-    Calendar \
     CalendarProvider \
     CaptivePortalLogin \
     CertInstaller \
