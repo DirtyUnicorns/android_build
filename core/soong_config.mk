2992e1d40f38bc221409a71b5870a002d2d8c959
Paul Keith <javelinanddart@gmail.com>
2018-07-21 00:20:21 -0500

    soong: Add var for linker shim libs
    * To limit security exposure, we're forcing all devices
      to define shim libs with a TARGET_LD_SHIM_LIBS
    
    Change-Id: Ic8722c42807429f2faa3546316c71c40533ce195

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -149,6 +149,7 @@ $(call add_json_bool, Needs_text_relocations,            $(filter true,$(TARGET_
 $(call add_json_bool, Has_legacy_mmap,                   $(filter true,$(BOARD_USES_LEGACY_MMAP)))
 $(call add_json_bool, Needs_prelink_support,             $(filter true,$(TARGET_NEEDS_PRELINK_SUPPORT)))
 $(call add_json_bool, Needs_non_pie_support,             $(filter true,$(TARGET_NEEDS_NON_PIE_SUPPORT)))
+$(call add_json_str,  Target_shim_libs,                  $(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 
 _contents := $(_contents)    "VendorVars": {$(newline)
 $(foreach namespace,$(SOONG_CONFIG_NAMESPACES),\
