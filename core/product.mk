417a7c70cc718ea63837f8eee7e9d8b82a350d88
dianlujitao <dianlujitao@lineageos.org>
2019-10-06 06:05:01 +0200

    Add PRODUCT_SOONG_NAMESPACES to _readonly_late_variables
    Change-Id: I28630e060e9223407c6e097b2a890ebd0e6e5722

--- core/product.mk
+++ core/product.mk
@@ -498,6 +498,7 @@ _readonly_late_variables += \
   PRODUCT_COPY_FILES \
   PRODUCT_DEX_PREOPT_NEVER_ALLOW_STRIPPING \
   PRODUCT_DEX_PREOPT_BOOT_FLAGS \
+  PRODUCT_SOONG_NAMESPACES \
 
 _readonly_early_variables := $(filter-out $(_readonly_late_variables),$(_product_var_list))
 
